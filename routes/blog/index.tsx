5ddb852d03a6dec0ef161bd2c69c6c9728fd1505
jmillana <jordi@marimon.tech>
2023-04-21 22:59:41 +0200

    Update blog main page

--- routes/blog/index.tsx
+++ routes/blog/index.tsx
@@ -1,9 +1,9 @@
 import { Head } from "$fresh/runtime.ts";
 import { Handlers, PageProps } from "$fresh/server.ts";
-import Counter from "../../islands/Counter.tsx";
 import { listPosts } from "../../utils/posts.ts";
 import Navbar from "../../components/Navbar.tsx";
 import Footer from "../../components/Footer.tsx";
+import type { Post } from "../../types.d.ts";
 import { CSS } from "$gfm/mod.ts";
 
 export const handler: Handlers = {
@@ -17,36 +17,33 @@ export default function Home(props: PageProps) {
   const { data } = props;
   const { posts } = data;
   const { dark_mode } = data;
-  var theme = "";
-
-  if (dark_mode) {
-    theme = "dark";
-  } else {
-    theme = "light";
-  }
 
   return (
-    <main
-      class="p-4"
-      data-color-mode={theme}
-      data-dark-theme={theme}
-      class="markdown-body"
-      style="display: flex; flex-direction: column; min-height: 100vh; padding-left: 5rem; padding-right: 5rem;"
-    >
-      <Navbar />
-      <style dangerouslySetInnerHTML={{ __html: CSS }} />
-      <h1 class="text-4xl font-bold">Latest Posts</h1>
-      {posts.map((post) => (
-        <article class="mt-4">
-          <h2 class="text-2xl font-bold">
-            <a class="hover:text-blue-600" href={`/blog/${post.id}`}>
-              {post.title}
-            </a>
-          </h2>
-          <time>{Intl.DateTimeFormat("es").format(post.date)}</time>
-        </article>
-      ))}
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <link rel="stylesheet" href="/styles/main.css" />
+      </Head>
+      <main
+        class="p-4, markdown-body"
+        data-color-mode="dark"
+        data-dark-theme="dark"
+        style="display: flex; flex-direction: column; min-height: 100vh; padding-left: 5rem; padding-right: 5rem;"
+      >
+        <Navbar />
+        <style dangerouslySetInnerHTML={{ __html: CSS }} />
+        <h1 class="text-4xl font-bold">Latest Posts</h1>
+        {posts.map((post: Post) => (
+          <article class="mt-4">
+            <h2 class="text-2xl font-bold">
+              <a class="hover:text-blue-600" href={`/blog/${post.id}`}>
+                {post.title}
+              </a>
+            </h2>
+            <time>{Intl.DateTimeFormat("es").format(post.date)}</time>
+          </article>
+        ))}
+        <Footer />
+      </main>
+    </>
   );
 }
